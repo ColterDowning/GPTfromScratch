55d8c869499f5f9a9009343ee251c472719eae55
ColterDowning <colterdowning0@gmail.com>
2024-09-18 19:14:44 -0700

    Fully implemented the decoder transformer for tiny shakespeare! Beefy GPU needed to train this version

--- bigramv2.py
+++ bigramv2.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters. These are the parameters that we can tune to optimize our model. They are created by the developer (me) to control the behavior of the model.
-batch_size = 32
-block_size = 8
-max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+batch_size = 64 # number of independent sequences to process in parallel
+block_size = 256 # number of tokens in a sequence. If this is 256, the nn will generate the 257th token
+max_iters = 5000 # number of iterations to train for
+eval_interval = 500
+learning_rate = 3e-4
 eval_iters = 200
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 #-------------
 
 torch.manual_seed(1337)
@@ -66,6 +69,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B,T,C = x.shape
@@ -75,6 +79,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * C**-0.5 # (B,T,C) @ (B,C,T) -> (B,T,T)
         wei = wei.masked_fill(self.tril[:T, :T]==0, float('-inf')) # (B,T,T)
         wei = F.softmax(wei, dim=-1) # (B,T,T)
+        wei = self.dropout(wei)
         # perform the weighted aggregation of the values
         v = self.value(x)
         out = wei @ v # (B,T,T) @ (B,T,C) -> (B,T,C)
@@ -86,21 +91,41 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
-    
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
 class FeedForward(nn.Module):
     # a simple linear layer followed by a non-linearity
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
         )
-        
     def forward(self, x):
         return self.net(x)
+    
+class Block(nn.Module):
+    def __init__(self, n_embd, n_head):
+        #n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+        
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
 
         
 class BigramLanguageModel(nn.Module):
@@ -110,8 +135,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = MultiHeadAttention(4, n_embd//4) # 4 heads of 8 dimensional self attention
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -120,8 +145,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C) which is batch, time, channel 
         pos_emb = self.position_embedding_table(torch.arange(T)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.sa_head(x) # apply the 4 heads of self attention (B,T,C)
-        x = self.ffwd(x) # (B,T,C)
+        x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
